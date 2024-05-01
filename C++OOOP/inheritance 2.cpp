72fe24aae41c50c7d92829b8bac816038a94feb2
kessie6 <168116521+kessie6@users.noreply.github.com>
2024-05-01 15:41:30 +0700

    Add files via upload

---
+++ C++OOOP/inheritance 2.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+using namespace std;
+class Base{
+protected:
+    int x,y;
+Base(){
+x = 0 ;
+y = 0 ;
+}
+Base(int x,int y){
+this -> x = x ;
+this -> y = y;
+}
+void input(){
+cout<<"Enter Value X : ";cin>>x;
+cout<<"Enter Value Y :  ";cin>>y;
+}
+void output(){
+cout<<"X = "<<x<<endl;
+cout<<
+}
+
+
+};
